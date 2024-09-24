3f3ceb2ffb336d987711395989526cd6a38652eb
Andronique <andronique64@gmail.com>
2024-09-24 09:35:47 +0300

    second commit

--- nodejs-authentification-projet-initial/app.js
+++ nodejs-authentification-projet-initial/app.js
@@ -1,31 +1,126 @@
+require("dotenv").config();
+
+
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
 const fs = require("fs");
-const { cours } = require("./data");
+const { cours, utilisateurs } = require("./data");
+const session = require('express-session')
+const bcrypt = require('bcrypt')
 
 const app = express();
 
 app.engine("html", ejs.__express);
+
+app.use(session({
+  name: process.env.SESSION_NAME,
+  resave: false,
+  saveUninitialized: false,
+  secret : process.env.SESSION_SECRET ,
+  cookie: {
+    maxAge: 1000 * 60 * 60 *24*7,
+    secure: false,
+  }
+}))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "html");
 app.set("views", path.join(__dirname, "views"));
 
+const protectionRoute = (req , res , next) =>{
+  if(!req.session.idUtilisateur){
+    res.redirect("/connexion")
+  }else{
+    next()
+  }
+}
+
+
+app.use((req, res, next)=>{
+  const  {idUtilisateur} = req.session
+  if(idUtilisateur){
+    res.locals.utilisateur = utilisateurs.find((utilisateur)=>utilisateur.id === idUtilisateur)
+  }
+  next();
+})
+
+
 app.get("/", (req, res) => {
-  res.render("index", { cours });
+  const {utilisateur} = res.locals
+  res.render("index", { cours , utilisateur});
 });
 app.get("/connexion", (req, res) => {
   res.render("connexion");
 });
+
+app.post("/connexion" , async(req , res)=>{
+  const {email, password} = req.body;
+  if(email && password) {
+    const utilisateur = utilisateurs.find((utilisateur)=>utilisateur.email===email)
+    if(utilisateur){
+      const validPw = await bcrypt.compare(password , utilisateur.password)
+
+      if(validPw){
+        req.session.idUtilisateur= utilisateur.id
+        return res.render("index" , {cours})
+      }else{
+        console.log("Mot de passe incorrect")
+      }
+    }else{
+      console.log("Utilisateur n'existe pas");
+    }
+  }
+
+  res.redirect("/connexion")
+
+})
+
+
 app.get("/inscription", (req, res) => {
   res.render("inscription");
 });
 
-app.get("/lectureVideo", (req, res) => {
-  res.render("lectureVideo");
+app.post("inscription" , async(req , res)=> {
+  const {nom , email , password} = req.body;
+if(nom && email  && password){
+  const utilisateur = utilisateurs.some((utilisateur)=>utilisateur.email === email);
+  if(!utilisateur){
+    const salt = await bcrypt.genSalt(10)
+    const pwToSave = await bcrypt.hash(password , salt)
+
+    let nouvelUtilisateur = {
+      id: utilisateurs.length +1,
+      nom, email,
+      password: pwToSave
+    }
+    utilisateurs.push(nouvelUtilisateur);
+    req.session.idUtilisateur = nouvelUtilisateur.id;
+    return res.render("index", {cours , utilissateur: nouvelUtilisateur})
+  }
+
+}
+res.redirect("/inscription")
+})
+app.get("/lectureVideo", protectionRoute , (req, res) => {
+  const {utilisateur} = res.locals
+  res.render("lectureVideo" , {utilisateur});
 });
+
+
+app.post("/deconnexion" , (req, res)=> {
+  req.session.destroy((err)=>{
+    if(err){
+      return res.render("index" , {cours})
+    }
+    res.clearCookie(process.env.SESSION_NAME);
+    return res.render('index', {cours})
+  })
+})
 
 app.get("/video", (req, res) => {
   const range = req.headers.range;
